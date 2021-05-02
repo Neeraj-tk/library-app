12a1a85ff989d1ba89500009fbf5dd4fd89e1c2a
neeraj <manuniji@gmail.com>
2021-05-02 09:50:00 +0300

    2/5-2.0

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/librarydb");
+
+const userSchema= new mongoose.Schema({username:String,
+password:String,
+email:String});
+
+const User=mongoose.model("User",userSchema);
 
 const app=express();
 
@@ -11,10 +20,38 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",function(req,res){
-   // res.sendFile(__dirname+"/views/welcome.ejs");
     res.render("welcome");
 });
 
+app.get("/signup",function(req,res){
+    res.render("signup");
+});
+
+app.post("/signup",function(req,res){
+const newUser=new User({username:req.body.username,email:req.body.email,password:req.body.password});
+newUser.save();
+});
+
+app.post("/",function(req,res){
+    const name=req.body.username;
+    User.find({username:name},function(err,user){
+       if(!user)
+       {
+           if(req.body.password===user.password)
+           {
+               res.render("/home");
+           }
+           else
+           {
+               res.render("/",{message:"Incorrect Password"});
+           }
+       }
+       else
+       {
+           res.render("/",{message:"Incorrect username"});
+       }
+    });
+});
 
 app.listen(3000,function(){
     console.log("server started");
