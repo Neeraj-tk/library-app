bfda7e4215dd10be55772b2ea08f4489fbb8b4e0
Harshi194 <hpillarisetti@students.vnit.ac.in>
2021-05-02 10:54:52 +0400

    2/5-3.0

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ app.set('view engine','ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+message="";
 
 app.get("/",function(req,res){
    // res.sendFile(__dirname+"/views/welcome.ejs");
     res.render("welcome");
 });
 
+
+app.get("/signup", function(req, res){
+    res.render("signup");
+});
+
+app.post("/signup", function(req, res){
+    console.log("registered");
+});
 
 app.listen(3000,function(){
     console.log("server started");
